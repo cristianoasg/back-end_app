642f1016250353a0cf5e1bcb2ed8e597de470605
Cristiano Augusto <cristiano.asg@hotmail.com>
2020-11-18 20:29:32 +0000

    creation of services

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import appointmentsRouters from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouters);
+routes.use('/users', usersRouter);
 
 export default routes;
